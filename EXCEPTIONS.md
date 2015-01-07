5893ef44fe15f4737a5a21b1f8ff9c945deb98d7
n5xm <evangel_a@163.com>
2015-01-07 17:29:30 +0800

    Completed hibernate_annotations

---
+++ EXCEPTIONS.md
@@ -0,0 +1,16 @@
+# EXCEPTIONS
+
+------
+
+## 1. NoSuchFieldError
+
+> `Caused by: java.lang.NoSuchFieldError: INSTANCE`
+
+> hibernate3.5以后就集成annotation了，不需要引入hibernate-annotations.jar和hibernate-commons-annotations.jar，在项目中去掉这两个jar包，就可以了
+
+## 2. ClassNotFoundException
+
+> `Caused by: java.lang.ClassNotFoundException: javax.persistence.Cacheable`
+
+> 需要加入hibernate-distribution-3.5.0-Final/lib/jpa目录下的hibernate-jpa-2.0-api-1.0.0.Final.jar
+
---
+++ src/com/yiibai/EmployeeAnnotations.java
@@ -0,0 +1,57 @@
+package com.yiibai;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee_annotations")
+public class EmployeeAnnotations {
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "salary")
+	private int salary;
+
+	public EmployeeAnnotations() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String first_name) {
+		this.firstName = first_name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String last_name) {
+		this.lastName = last_name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+}
