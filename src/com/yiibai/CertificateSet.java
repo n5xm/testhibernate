d27d2bb05c806a340f48de12922ac2f58094e878
n5xm <evangel_a@163.com>
2015-01-08 10:59:58 +0800

    Completed hibernate_set_mapping

---
+++ src/com/yiibai/CertificateSet.java
@@ -0,0 +1,47 @@
+package com.yiibai;
+
+public class CertificateSet {
+	private int id;
+	private String name;
+
+	public CertificateSet() {
+	}
+
+	public CertificateSet(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (!this.getClass().equals(obj.getClass()))
+			return false;
+		CertificateSet obj2 = (CertificateSet) obj;
+		if ((this.id == obj2.getId()) && (this.name.equals(obj2.getName()))) {
+			return true;
+		}
+		return false;
+	}
+
+	public int hashCode() {
+		int tmp = 0;
+		tmp = (id + name).hashCode();
+		return tmp;
+	}
+}
